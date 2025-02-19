cdd1a1318faa22046448b8735d264d65704e2e9a
Boluwatife Dada <boluwatife.dada@stu.ocsb.ca>
2025-02-19 17:46:55 +0000

    area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Boluwatife Dada
+# Date: feb 18, 2025
+# This program is calculating the Area & Perimeter of a  Rectangle.
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm")
+    print("")
+    print("This area {}".format(5 * 3))
+    print("The perimeter is : {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
